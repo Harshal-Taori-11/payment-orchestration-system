ff7c4ef5fdc78389c2526bdd52ee48ae5ca90b12
Harshal Taori <harshaltaory1111@gmail.com>
2026-05-27 19:30:57 +0530

    Project setup infor Java-Maven - Initial Commit

---
+++ src/main/java/com/payment/payment_orchestration_system/PaymentOrchestrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.payment.payment_orchestration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentOrchestrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentOrchestrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/payment_orchestration_system/PaymentOrchestrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.payment_orchestration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentOrchestrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
